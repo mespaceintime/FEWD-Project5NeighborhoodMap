2f2be39018b1da8ff92b25d6bd2b6cfbbaa66840
mespaceintime <mespaceintime@gmail.com>
2015-10-14 08:26:10 -0400

    revised code

---
+++ README.md
@@ -0,0 +1,114 @@
+frontend-nanodegree-Project-5-Neighborhood-Map
+===============================
+###**Instructions: How to Use This App:**
+
+Open the index.html in your browser.
+
+Use the search bar to select food type and destination city, state.
+
+View the customized Google Maps.
+
+See site map pins about the neighborhood.
+
+Search bar is used to find points of interest and display destination details.
+
+Select from the list of eateries.
+
+===============================
+**Goal of the Project:**
+* The goal of this project is to learn about  Google Maps API using Javascript and knockout.
+
+* This project uses the Google Maps API to orient the location of desireable destinations.
+
+**Results:**
+
+A clean running map which meets specifications.
+
+###Resources Used:
+
+Required files:
+index.html
+js/app.js
+js/knockout.js
+css/style.css
+
+###Install
+*  MAC Terminal $ npm install --save yelp
+*  $ npm install oauth
+
+* [Yelp](https://arian.io/how-to-use-yelps-api-with-node/)
+*  $ npm install --save oauth-signature  
+*  $ npm install --save nonce  
+*  $ npm install --save request  
+
+* [Bootstrap](http://getbootstrap.com/getting-started/#download)
+* [Font-Awesome](http://fortawesome.github.io/Font-Awesome/)
+* [Font Icons](https://fonticons.com)
+* [Map Icons](http://map-icons.com/#)
+
+**Udacity - Primary Courses**
+* [Udacity](https://www.udacity.com/)
+* [Intro to AJAX](https://www.udacity.com/course/viewer#!/c-ud110-nd)
+* [JavaScript Design Patterns](https://www.udacity.com/course/viewer#!/c-ud989-nd)
+* [Udacity Discussion Forum](http://discussions.udacity.com/)
+* [Piazza Forums](https://piazza.com/class/i36sqlrb9xu332)
+* [Google Search "js validator"](https://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=js+validator)
+* [youtube](https://www.youtube.com/watch?v=RyC5siYWrUc)
+* [TMDb](API Overview - The Movie Database-TMDb)
+* [Apiary](http://docs.themoviedb.apiary.io/)
+* [Apiary](https://github.com/feliu-io/fewd-p5)
+
+**Resources**
+* [Google Maps Street View Service](https://developers.google.com/maps/documentation/javascript/streetview)
+* [Google Maps](https://developers.google.com/maps/)
+* [Yelp](https://www.yelp.com/)
+* [Yelp API](https://www.yelp.com/developers/documentation/v2/overview)
+* [Yelp](http://gis.yohman.com/up206b/tutorials/api-access-yelp/)
+* [Yelp Search API](https://www.yelp.com/developers/documentation/v2/search_api)
+* [Yelp Business API](https://www.yelp.com/developers/documentation/v2/search_api)
+* [Yelp Phone Search API](https://www.yelp.com/developers/documentation/v2/phone_search)
+* [Obtain a Google Maps API Key](http://www.google.com/apis/maps/signup.html)
+* [Obtain a "Yelp Web Services ID(ywsid)](http://www.yelp.com/developers/getting_started/api_access)
+* [Yelp API Documentation](http://www.yelp.com/developers/documentation)
+* [Yelp API Support Forum](http://groups.google.com/group/yelp-developer-support)
+* [Google Maps Javascript API](https://developers.google.com/maps/documentation/javascript/tutorial)
+* [KO](http://knockoutjs.com/documentation/introduction.html)
+* [StackOverflow](http://stackoverflow.com/questions/22052674/google-places-api-city-or-zipcode)
+* [Mozilla Developer Network](https://developer.mozilla.org/en-US/)
+* [jshint](http://jshint.com/) - JS validation
+* [jslint](http://jslint.com/)- JS validation
+* [jslint](http://www.htmllint.net/en/html-lint/htmllintl.html)- HTML validation
+* [csslint](http://csslint.net)- CSS validation
+* [Google Maps API](https://developers.google.com/maps/documentation/javascript/examples/map-simple)
+* [json](http://ip-api.com/json)
+* [json](http://json-jsonp-tutorial.craic.com/index.html)
+
+* Learning Web Design - Jennifer Niederst Robbins
+* [jsfiddle](http://jsfiddle.net/haridarshan/SM9pR/11/)
+* [jsfiddle](http://jsfiddle.net/rniemeyer/aDahT/)
+* [stackoverflow](http://stackoverflow.com/)
+* [stackoverflow](http://stackoverflow.com/questions/20939846/mvc4-knockout-googlemap-binding-error)
+* [stackoverflow](http://stackoverflow.com/questions/12722925/google-maps-and-knockoutjs)
+* [codeproject](http://www.codeproject.com/Articles/351298/KnockoutJS-and-Google-Maps-binding)
+*[Google Maps pin editor](http://google-maps-utility-library-v3.googlecode.com/svn/trunk/styledmarker/)
+* [knockout framework](http://knockoutjs.com/documentation/introduction.html)
+JavaScript library Knockout allows users to organize code and data using the Model, View, and View Model (MVVM) method. 
+* [knockout](http://learn.knockoutjs.com/#/?tutorial=custombindings)
+* [knockout Plugins](https://github.com/knockout/knockout/wiki/Plugins)
+* [knockout Plugins](https://github.com/knockout/knockout/wiki/Plugins)](http://www.codeproject.com/Articles/387626/BikeInCity-2-KnockoutJS-JQuery-Google-Maps)
+* [knockout](http://jsfiddle.net/Wt3B8/23/)
+* [knockout](http://jsfiddle.net/t9wcC/)
+* [knockout](http://www.codeproject.com/Articles/755386/Jumpstart-KnockoutJS-with-ASP-NET-MVC-Web-Api)
+* [w3schools](http://www.w3schools.com/) - JavaScript training and code validation.
+* [w3schools html5_geolocation.asp](http://www.w3schools.com/html/html5_geolocation.asp)
+* [google dev](https://developers.google.com/maps/documentation/javascript/examples/marker-animation)
+* [google dev](Click event listener - https://developers.google.com/maps/documentation/javascript/events)
+* [google map api](http://stackoverflow.com/questions/8049029/move-zoom-bar-to-the-right-with-google-maps-api-v3)
+* [googel reference](https://developers.google.com/maps/documentation/javascript/reference?hl=en)
+* [CSS tricks](https://css-tricks.com/forums/topic/viewport-height-for-full-screen-div)
+* [HTML5 Geolocation](http://www.tutorialspoint.com/html5/html5_geolocation.htm)
+* [jquery](http://api.jquery.com/)
+* [github](https://github.com/)
+* [Google Maps Garage: Where's My Map!](https://www.youtube.com/watch?v=21PDd17dnnI#t=62)
+
+* Thanks you so much to Udacity, Alexander Verge, scottyoon221, oped, davidmelhart, sahelmastoureshgh, Javier Feliu, and other classmates for the help!
---
+++ source code copy/app.js
@@ -0,0 +1,235 @@
+// Global Map and InfoWindow References
+var map;
+var infoWindow;
+
+// ViewModel in MVVM
+function ViewModel () {
+	var self = this;
+	var place;
+	// Search box 
+	self.currentLocation = function() {
+		var location = $("#location:input").val();
+		return location;
+	};
+	self.currentFilter = ko.observable();
+	self.placesList = ko.observableArray([]);
+	
+    // Map
+	var markersArray = [];
+	function mapRender() {
+
+		var mapOptions = {
+			zoomControl: true,
+			zoomControlOptions: {
+				style: google.maps.ZoomControlStyle.LARGE,
+				position: google.maps.ControlPosition.RIGHT_TOP
+			},
+			scaleControl: true,
+			streetViewControl: true,
+			streetViewControlOptions: {
+				position: google.maps.ControlPosition.RIGHT_TOP
+			},
+			panControl: true,
+			mapTypeControl: true,
+			//http://www.w3schools.com/googleAPI/ref_setmaptypeid.asp
+			MapTypeId: google.maps.MapTypeId.TERRAIN
+		};
+
+		map = new google.maps.Map(document.getElementById("gMap"), mapOptions);
+
+		var defaultBounds = new google.maps.LatLngBounds(
+	    	new google.maps.LatLng(25, -125), 
+	    	new google.maps.LatLng(50, -55)
+		);
+		map.fitBounds(defaultBounds);
+		
+		// Options
+		var listDestinations = (document.getElementById("places"));
+		listDestinations.index = 5;
+		map.controls[google.maps.ControlPosition.LEFT].push(listDestinations);
+
+		// Nav
+		var naviForm = (document.getElementById("navigation"));
+		naviForm.index = 1;
+		map.controls[google.maps.ControlPosition.LEFT].push(naviForm);
+
+		// Search
+		var locName = document.getElementById("location");
+
+		var locData = {
+			types: ['(cities)'],
+			componentRestrictions: {country: "us"}
+		};
+
+		var searchBox = new google.maps.places.SearchBox(locName, locData);
+
+		var placesArray = ko.observableArray([]);
+
+	// Yelp - http://forum.jquery.com/topic/hiding-oauth-secrets-in-jquery, with gratitude to Jim(coldfusionguy).
+
+		function yelpRequest (){
+			var auth = {
+				consumerKey: "3mVw_1SgwXXaOPQR3_b6nA", 
+				consumerSecret: "jvsTuKvAKvzJ9xdPJ0y3ok1M-nw",
+				accessToken: "yvAfqbwdRsRpTej0DDv_xNy5F7cC4K7n",
+				accessTokenSecret: "bX1kMJoe2yT4VUmZaYxeaJV56eM",
+				serviceProvider: { 
+				signatureMethod: "HMAC-SHA1"
+				}
+			};
+
+			var eats = self.currentFilter();
+			var near = self.currentLocation();
+
+			var accessor = {
+				consumerSecret: auth.consumerSecret,
+				tokenSecret: auth.accessTokenSecret
+			};
+
+			parameters = [];
+			parameters.push(['eat', eats]);
+			parameters.push(['location', near]);
+			parameters.push(['callback', 'cb']);
+			parameters.push(['oauth_consumer_key', auth.consumerKey]);
+			parameters.push(['oauth_consumer_secret', auth.consumerSecret]);
+			parameters.push(['oauth_token', auth.accessToken]);
+			parameters.push(['oauth_signature_method', 'HMAC-SHA1']);
+
+			var message = { 
+				'action': 'http://api.yelp.com/v2/search',
+				'method': 'GET',
+				'parameters': parameters 
+			};
+
+			OAuth.setTimestampAndNonce(message);
+			OAuth.SignatureMethod.sign(message, accessor);
+
+			var parameterMap = OAuth.getParameterMap(message.parameters);
+			parameterMap.oauth_signature = OAuth.percentEncode(parameterMap.oauth_signature);
+
+			// Error handling
+			var errorHandler = setTimeout(function(){
+				alert("Your request search could not be processed. Please check your internet connection.");
+			}, 2000);
+
+			$.ajax({
+				'url': message.action,
+				'data': parameterMap,
+				'cache': true,
+				'dataType': 'jsonp',
+				'jsonpCallback': 'cb',
+				'success': function(data, textStats, XMLHttpRequest) {
+					clearTimeout(errorHandler);
+					placesArray().push(data.businesses);
+				},
+				// On completion of the request the function for rendering fires //
+				'complete': function () {
+				  	var bounds = new google.maps.LatLngBounds();
+					var places = placesArray()[0];	
+				
+				// Maker	
+					for (place in placesArray()[0]) {
+						insertMarker(places);
+
+						placesArray()[0][place].address = (placesArray()[0][place].location.display_address[0] 
+							+ ", " + placesArray()[0][place].location.display_address[2]).toString(); 
+				// No image  //
+
+						if (placesArray()[0][place].image_url === undefined) {
+							placesArray()[0][place].image_url = "http://placehold.it/5x5";
+						}
+						self.placesList.push(placesArray()[0][place]);
+
+				
+				// Map bounds per markers
+						bounds.extend(markersArray[place].position);
+						map.fitBounds(bounds);
+					}
+
+					
+				}
+			});
+		}
+
+		document.getElementById("nav-btn").addEventListener("click", function() {
+			// Clear 
+			clearOverlays();
+			yelpRequest();
+		});
+
+	// Source - http://stackoverflow.com/questions/1544739/gMaps-api-v3-how-to-remove-all-markers
+	// IClear previous markers
+		function clearOverlays() {
+		  for (var i = 0; i < markersArray.length; i++ ) {
+		    markersArray[i].setMap(null);
+		  }
+		  markersArray.length = 0;
+		  self.placesList([]);
+		  placesArray().length = 0;
+		}
+	// Error handling
+		// var mapErrorHandler = setTimeout(function(){
+		// 	alert("Page unavailable.  Lost connection.");
+		// }, 2500); */
+	 //    google.maps.event.addListener(map, 'new', function() {
+	 //    	clearTimeout(mapErrorHandler);
+	 //    });
+	} 
+
+	google.maps.event.addDomListener(window, 'load', mapRender);
+
+	// infoWindow
+	var infoWindow = new google.maps.InfoWindow();
+	
+	function insertMarker(places){
+		
+		// Marker
+		var marker = new google.maps.Marker({
+			id: places[place].id,
+			map: map,
+			position: new google.maps.LatLng((places[place].location.coordinate.latitude),(places[place].location.coordinate.longitude)),
+			title: places[place].name,
+			animation: google.maps.Animation.DROP,
+		//  Content
+			content: "<div style='text-align: center'><b>" +
+				places[place].name + "</b></br>" +
+				"<img src=" + places[place].image_url + "></br>" +
+				places[place].location.display_address[0]});
+		markersArray.push(marker);
+
+		// Click event listener
+		google.maps.event.addListener(marker, "click", function() {
+			infoWindow.setContent(marker.content);
+			infoWindow.open(map, this);
+			marker.setAnimation(google.maps.Animation.DROP);
+		});
+	}
+
+	self.listClick = function(place) {
+		var marker; 
+	// Select marker
+		for (marker in markersArray) {
+			if (markersArray[marker].id === place.id){
+	// Pick from markersArray		
+				marker = markersArray[marker];
+				// Stop searching upon selection click
+				break; 
+			}
+		}
+	// Smooth map transition animation
+		map.panTo(marker.position);
+		
+		var dataPlaces =
+				"<div style='text-align: center'><b>" +
+				place.name + "</b></br>" +
+				"<img src=" + place.image_url + "></br>" +
+				place.address +  
+				"</div>";
+        //https://developers.google.com/maps/documentation/javascript/examples/infowindow-simple
+		infoWindow.open(map, marker);
+		infoWindow.setContent(dataPlaces);
+		marker.setAnimation(google.maps.Animation.DROP);
+	};
+}
+// Apply knockOut bindings
+ko.applyBindings(new ViewModel());
